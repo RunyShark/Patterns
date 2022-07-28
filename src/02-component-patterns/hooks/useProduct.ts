eea24adc366de4005d9768d61f4f8f779cf9cd8c
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-27 19:04:56 -0500

    hoy

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,8 +1,19 @@
 import { useState } from "react";
-export const useProduct = () => {
+import { Product, onChangeArg } from "../interfaces/interface";
+
+interface useProductArgs {
+  product: Product;
+  onChange?: (args: onChangeArg) => void;
+}
+
+export const useProduct = ({ onChange, product }: useProductArgs) => {
   const [countet, setCountet] = useState(0);
   const increaseBy = (value: number) => {
-    setCountet((prev) => Math.max(prev + value, 0));
+    const newValue = Math.max(countet + value, 0);
+
+    setCountet(newValue);
+
+    onChange && onChange({ count: newValue, product });
   };
 
   return {
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -4,10 +4,41 @@ import {
   ProductImage,
   ProductTitle,
 } from "../components";
-import { products } from "./products";
+//import { products } from "./products";
 import "../styles/custom-styles.css";
+import { Product } from "../interfaces/interface";
+import { useState } from "react";
+const product1 = {
+  id: 1,
+  title: "Coffe Mug",
+  img: "./coffee-mug.png",
+};
+const product2 = {
+  id: 2,
+  title: "Coffe Mug meme",
+  img: "./coffee-mug2.png",
+};
+const products: Product[] = [product1, product2];
+
+interface ProductInCart extends Product {
+  count: number;
+}
 
 export const ShoppingPage = () => {
+  const [shuppigCart, setShuppigCart] = useState<{
+    [key: number]: ProductInCart;
+  }>({});
+
+  const onProductCountChange = ({
+    count,
+    product,
+  }: {
+    count: number;
+    product: Product;
+  }) => {
+    console.log(product);
+  };
+
   return (
     <div>
       <h1>ShoppingPage</h1>
@@ -20,7 +51,12 @@ export const ShoppingPage = () => {
         }}
       >
         {products.map((product) => (
-          <ProductCard key={product.id} product={product} className="bg-dark">
+          <ProductCard
+            key={product.id}
+            product={product}
+            className="bg-dark"
+            onChange={onProductCountChange}
+          >
             <ProductImage className="custom-image" />
             <ProductTitle className="text-white" />
             <ProductButton className="custom-buttoms" />
